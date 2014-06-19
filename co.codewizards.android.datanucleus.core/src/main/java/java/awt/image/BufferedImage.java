74125dd48c4362ebfee358333b528e26eaa301b4
Marco Nguitragool-Schulze <marco@codewizards.co>
2014-06-19 17:20:52 +0200

    Added BufferedImage.

---
+++ co.codewizards.android.datanucleus.core/src/main/java/java/awt/image/BufferedImage.java
@@ -0,0 +1,9 @@
+package java.awt.image;
+
+/**
+ * Dummy class to fulfill DataNucleus' extension.
+ * @author Marco หงุ่ยตระกูล-Schulze - marco at codewizards dot co
+ */
+public class BufferedImage {
+
+}
